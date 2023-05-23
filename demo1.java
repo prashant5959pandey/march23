02a6fd9daef12b416502f4a0aa1bbcdb676bec63
Prashant Pandey <prashantpandey5959@gmail.com>
2023-05-23 14:25:11 +0530

    commit is done

--- demo1.java
+++ demo1.java
@@ -2,7 +2,7 @@ class demo1{
     public static void main(String args[]){
         int a=10;
         int b=2;
-        int sum = a+b;
+        int sum = a+b+a;
         System.out.printf(sum);
     }
 }
